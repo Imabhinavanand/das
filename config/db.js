715baee292fa96d04fa5cd783172ebe6b11714b0
ABHINAV ANAND <37950680+Imabhinavanand@users.noreply.github.com>
2023-07-27 20:33:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://learning:DcGtmPHw7kyKveNv@cluster0.t6fgp1f.mongodb.net/");
+    await mongoose.connect("mongodb+srv://learning:GQYt8dQLYgclgYOx@cluster0.t6fgp1f.mongodb.net/doctor-app");
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
